2a550e0cd5ac2052156f1bb730f1a855f447a5f7
Leehojun12 <dlghwns82@naver.com>
2022-02-24 12:20:30 +0900

    undate

---
+++ Day01/src/Exam02_Intro.java
@@ -0,0 +1,14 @@
+
+public class Exam02_Intro {
+	public static void main(String[]args) {
+		// 자바에서 "" : 문자열(0글자 이상)
+		// '' : 문자(한 글자만 사용 가능)
+		// 자바에서 숫자는 그냥 써도 무방
+		System.out.println("a");
+		System.out.println('가');
+		System.out.println("가나다");
+		System.out.println(50);
+		System.out.println(0.21);
+		System.out.println(-5.212);
+	}
+}

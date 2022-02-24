e945563df6cc65d42f469b9a9c6601ea5b2eb6e3
Leehojun12 <dlghwns82@naver.com>
2022-02-24 15:32:01 +0900

    Update

--- Day01/src/Exam02_Intro.java
+++ Day01/src/Exam02_Intro.java
@@ -4,11 +4,24 @@ public class Exam02_Intro {
 		// 자바에서 "" : 문자열(0글자 이상)
 		// '' : 문자(한 글자만 사용 가능)
 		// 자바에서 숫자는 그냥 써도 무방
-		System.out.println("a");
-		System.out.println('가');
-		System.out.println("가나다");
-		System.out.println(50);
-		System.out.println(0.21);
-		System.out.println(-5.212);
+		
+//		System.out.println("a");
+//		System.out.println('가');
+//		System.out.println("가나다");
+//		System.out.println(50);
+//		System.out.println(0.21);
+//		System.out.println(-5.212);
+		
+		// 실습 1 : 인삿말, 본인의 이름, 영어 이름, 나이 , 체온 측정결과
+//		System.out.println("안녕하세요?");
+//		System.out.print("저의 이름은 이호준입니다. ");
+//		System.out.print("영어 이름은 Lee ho jun입니다.");
+//		System.out.println("\n 제 나이는 28입니다.");
+//		System.out.println("오늘의 체온 측정 결과는 36.5도입니다.");
+		
+		// + : 문자열 연결
+		// : 순서에 따라 문자와 숫자가 연결되어 문자로 합쳐질 수도, 숫자와 숫자가 더해져 덧셈이 될 수도 있음
+		//System.out.println("안녕하세요?"+ "\n저의 이름은 이호준입니다."+"영어 이름은 Leehojun입니다." + "\n 제 나이는 28입니다." + "\n오늘의 체온 측정 결과는 36.5도입니다.");
+		System.out.println('a'+5+10);
 	}
 }
---
+++ Day01/src/Exam03_stock.java
@@ -0,0 +1,19 @@
+public class Exam03_stock {
+	public static void main(String[]args) {
+		System.out.println("========= 주식마켓 ========");
+		System.out.println("1. 삼성전자 (1주: "+ 40000+")");
+		System.out.println("2. 테슬라 (1주: "+ 60000+")");
+		System.out.println("3. 카카오 (1주: "+ 30000+")");
+		System.out.println("잔고 : "+ 200000+"원");
+		System.out.println("=========================");
+		
+		System.out.println("삼성전자 1주 매수");
+		System.out.println("잔고 : "+ (200000 - 40000) +"원");
+		
+		System.out.println("테슬라 2주 매수");
+		System.out.println("잔고 : " +(200000 - 40000 - 120000) +"원");
+		
+		System.out.println("카카오 1주 매수");
+		System.out.println("잔고 : " + (200000- 40000 - 120000 - 30000)+"원");
+	}
+}

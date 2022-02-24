9bf58e7aadc8800b78a97bf9cdb9cdd044d5c4e0
Leehojun12 <dlghwns82@naver.com>
2022-02-24 11:56:51 +0900

    first commit

---
+++ Day01/src/Exam01_Print.java
@@ -0,0 +1,10 @@
+
+public class Exam01_Print {
+
+	public static void main(String[]args) {
+		// print(풀력) 구문 작성 - A, B, C
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+	}
+}

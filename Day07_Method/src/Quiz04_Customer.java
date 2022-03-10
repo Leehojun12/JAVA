ee3afe8ffcc1636fc0d48bf443284078c67cb467
Leehojun12 <dlghwns82@naver.com>
2022-03-10 10:45:01 +0900

    Update

--- Day07_Method/src/Quiz04_Customer.java
+++ Day07_Method/src/Quiz04_Customer.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class Quiz04_Customer {
 
-	public static void getCustomer(String name) {
-		System.out.println(name + "님 어서오세요!");
+	public static String getCustomer(String name) {
+		return name + "님 어서오세요!";
 	}
 
-	public static void getCustomer(int num) {
-		System.out.println("손님" + num + "명 입장하였습니다.");
+	public static String getCustomer(int num) {
+		return "손님" + num + "명 입장하였습니다.";
 	}
 
-	public static void getCustomer(double temp) {
+	public static String getCustomer(double temp) {
 		if (temp < 35.0)
-			System.out.println("체온이 너무 낮습니다.");
+			return "체온이 너무 낮습니다.";
 		else if (temp >= 36 && temp <= 36.7)
-			System.out.println("정산 체온입니다.");
+			return "정산 체온입니다.";
 		else {
-			System.out.println("체온이 너무 높습니다.");
+			return "체온이 너무 높습니다.";
 		}
 	}
 
-	public static void getCustomer(boolean order) {
+	public static String getCustomer(boolean order) {
 		if (order == true) {
-			System.out.println("매장 손님입니다.");
-		} else if (order == false) {
-			System.out.println("포장 손님입니다.");
+			return "매장 손님입니다.";
+		} else{
+			return "포장 손님입니다.";
 		}
 	}
 
@@ -34,16 +34,16 @@ public class Quiz04_Customer {
 		System.out.println("=== 손님 맞이 프로그램 ===");
 		System.out.print("1. 이름을 입력해 주세요 >> ");
 		String name = sc.nextLine();
-		getCustomer(name);
+		System.out.println(getCustomer(name));
 		System.out.print("2. 인원수를 입력해 주세요 >> ");
 		int num = Integer.parseInt(sc.nextLine());
-		getCustomer(num);
+		System.out.println(getCustomer(num));
 		System.out.print("3. 현재 체온을 입력해 주세요. >> ");
 		double temp = sc.nextDouble();
-		getCustomer(temp);
+		System.out.println(getCustomer(temp));
 		System.out.print("4. 매장식사 여부를 입력해 주세요 >> ");
 		boolean order = sc.nextBoolean();
-		getCustomer(order);
+		System.out.println(getCustomer(order));
 
 		/*
 		 * 4가지 모두 getCustomer()라는 이름으로 메서드로 오버로딩을 적용 1. 사용자가 이름을 입력하면 '~님 안녕하세요!' 출력 -

1715d8b5945ff296ed92869605556d3345d70b42
Leehojun12 <dlghwns82@naver.com>
2022-03-17 18:10:34 +0900

    Update

--- Day10_OOP02/src/com/oop/shop01/Beverage.java
+++ Day10_OOP02/src/com/oop/shop01/Beverage.java
@@ -9,5 +9,8 @@ public class Beverage extends Product{
 	public Beverage(String product_no, String name, int price) {
 		super(product_no, name, price);
 	}
+	public double getRatePrice() {
+		return this.getPrice() *0.03;
+	}
 	
 }
--- Day10_OOP02/src/com/oop/shop01/Bread.java
+++ Day10_OOP02/src/com/oop/shop01/Bread.java
@@ -9,5 +9,7 @@ public class Bread extends Product{
 	public Bread (String product_no, String name, int price) {
 		super(product_no, name, price);
 	}
-	
+	public double getRatePrice() {
+		return this.getPrice() * 0.5;
+	}
 }

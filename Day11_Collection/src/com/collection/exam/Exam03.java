13fb789c0c50ddf5a0c70ae732292a5a94ddf55c
Leehojun12 <dlghwns82@naver.com>
2022-03-22 12:05:08 +0900

    Update

--- Day11_Collection/src/com/collection/exam/Exam03.java
+++ Day11_Collection/src/com/collection/exam/Exam03.java
@@ -28,17 +28,23 @@ public class Exam03 {
 		 * 기본 자료형 ArrayList
 		 * 기본 자료형 (boolean, char, byte, short, int, float, double, long)
 		 * -> 기본 자료형은 클래스가 X -> 자바에서 지정해놓은 특수한 기본 자료형
+		 * 
+		 * Wrapper Class
+		 * -> 기본 자료형들을 감싸고 있는 클래스 -> 기본 자료형을 마치 클래스처럼 사용하는게 가능하도록 해줌.
+		 * int -> Integer
+		 * double -> Double
+		 * float -> Float
+		 * char -> Character
 		 */
 		
 		String temp = "a";
 		int x = 10;
-		
-		
-		
-		
-		
+	
 		//int 형 arrayList 를 만들어보세요
 		ArrayList<Integer> ilist = new ArrayList<>();
+		ilist.add(10);
+		ilist.add(50);
+		System.out.println(ilist.get(0) + " : " + ilist.get(1));
 		
 		
 	}
